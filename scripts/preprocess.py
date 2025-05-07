e2fc5fe86566c254d736c05c80e11d37f27b1e55
WKAHPM <978088232@qq.com>
2025-05-07 09:23:19 +0800

    Change idiom data

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,10 +1,18 @@
 import json
 from utils import extract_pinyin
     
+with open("data/polyphones.json", "r", encoding="utf-8") as f:
+    polyphones_idioms = json.load(f)
 
+with open("data/polyphones_idioms.txt", "w", encoding="utf-8") as f:
+    for key in polyphones_idioms:
+        print(key, file=f)
 
 with open("data/idioms.txt", "r", encoding="utf-8") as f:
     idioms = [line.strip() for line in f if line.strip()]
+
+with open("data/polyphones_idioms.txt", "r", encoding="utf-8") as f:
+    idioms += [line.strip() for line in f if line.strip()]
 
 processed = []
 
