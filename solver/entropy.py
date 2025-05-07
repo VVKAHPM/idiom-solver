1a0baf5dd4e18b621b7497f378f0c559ebce8e15
WKAHPM <978088232@qq.com>
2025-05-07 10:14:30 +0800

    Add entropy calculator

--- solver/entropy.py
+++ solver/entropy.py
@@ -1,8 +1,23 @@
-from scripts.utils import extract_pinyin
+from solver.feedback_parser import get_feedback
+import math
 
-def judge_idiom(guess, result, idiom):
-    words, initials, finals, tones = extract_pinyin(guess) 
-    for i in range(0, 4):
+def feedback_to_key(feedback):
+    return tuple(tuple(x) for x in feedback)
 
+def calculate_entropy(guess, candidates):
+    distribution = {}
+    for candidate in candidates:
+        feedback = get_feedback(candidate, guess)
+        key = feedback_to_key(feedback)
+        if key in distribution:
+            distribution[key] += 1
+        else:
+            distribution[key] = 1
 
-def size_of_possible_idioms(condition, candidates):
+    result = 0.0
+    candidate_size = len(candidates)
+    for possible_result in distribution:
+        prob = distribution[possible_result] / candidate_size
+        result -= prob * math.log2(prob)
+
+    return result 
